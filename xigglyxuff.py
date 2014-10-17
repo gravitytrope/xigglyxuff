eccc7dc19c2eaaf8903dfe671eb768a90c2bca22
Igglybuff <ed.shelton@de.amadeus.com>
2014-10-17 11:15:09 +0200

    Create xigglyxuff.py
    Prints 10 random Xigglyxuff usernames in a super awesome and sexy way.

---
+++ xigglyxuff.py
@@ -0,0 +1,10 @@
+import random
+
+list = ['b', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 
+        'r', 's', 't', 'w', 'z', 'br', 'bl', 'p',
+        'dr', 'fl', 'fr', 'gl', 'gr', 'pl', 'pr',
+        'sn', 'sp', 'st', 'sw', 'tr', 'tw', 'wr',
+        ]
+
+for i in range(10):
+    print (''.join((random.choice(list).title(),'iggly',random.choice(list),'uff')))
