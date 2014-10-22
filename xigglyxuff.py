45e53b5613c6a376d8647838337b7b36a30e63b6
Ed Shelton <edsheltondezerith@gmail.com>
2014-10-22 16:25:42 +0200

    ERROR HANDLING MOTHERFUCKAZ :camel:

--- xigglyxuff.py
+++ xigglyxuff.py
@@ -11,17 +11,19 @@ list = ['b', 'd', 'f', 'g', 'j', 'l', 'm', 'n',
 print ('Hello, pokédude.')
 
 while True:
-    print ('\nHow many usernames do you want me to generate? (q to quit)\n')
-    u1 = input()
+    try:
+        print ('\nHow many usernames do you want me to generate? (q to quit)\n')
+        u1 = input()
 
-    if u1 == 'q':
-        sys.exit()
+        if u1 == 'q':
+            sys.exit()
 
-    u1 = int(u1)
-    for i in range(u1):
-        c1 = random.choice(list)
-        c2 = random.choice(list)
-        while c2 == c1:
+        u1 = int(u1)
+        for i in range(u1):
+            c1 = random.choice(list)
             c2 = random.choice(list)
-        print ('\t\t',c1.title()+'iggly'+c2+'uff')
-
+            while c2 == c1:
+                c2 = random.choice(list)
+            print ('\t\t',c1.title()+'iggly'+c2+'uff')
+    except ValueError:
+        print('\nOnly integers are accepted. Sort your life out.')
