5b202da8fdaa4482842b7aafbeb256827991901d
Ed Shelton <edsheltondezerith@gmail.com>
2014-10-22 17:39:29 +0200

    Boom, timed. :collision:

--- xigglyxuff.py
+++ xigglyxuff.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-import random
-import sys
+import random, sys, time
 
 list = ['b', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 
         'r', 's', 't', 'w', 'z', 'br', 'bl', 'p',
@@ -14,16 +13,23 @@ while True:
     try:
         print ('\nHow many usernames do you want me to generate? (q to quit)\n')
         u1 = input()
-
+        
         if u1 == 'q':
             sys.exit()
-
+            
+        start = time.time()
         u1 = int(u1)
+        
         for i in range(u1):
             c1 = random.choice(list)
             c2 = random.choice(list)
             while c2 == c1:
                 c2 = random.choice(list)
             print ('\t\t',c1.title()+'iggly'+c2+'uff')
+
+        end = time.time()
+        elapsed = end - start
+        
+        print('\nTime taken: ', elapsed, 'seconds.')
     except ValueError:
         print('\nOnly integers are accepted. Sort your life out.')
