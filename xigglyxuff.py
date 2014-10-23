888d4794a7c81fc10a9ce06544306db36617b3d9
Ed Shelton <edsheltondezerith@gmail.com>
2014-10-23 11:22:59 +0200

    Professor Oak's recommendations :tea:

--- xigglyxuff.py
+++ xigglyxuff.py
@@ -7,6 +7,8 @@ list = ['b', 'd', 'f', 'g', 'j', 'l', 'm', 'n',
         'sn', 'sp', 'st', 'sw', 'tr', 'tw', 'wr',
         ]
 
+
+
 print ('Hello, pokédude.')
 
 while True:
@@ -19,17 +21,24 @@ while True:
             
         start = time.time()
         u1 = int(u1)
+        xigList = []
         
         for i in range(u1):
+            xig = ''
             c1 = random.choice(list)
             c2 = random.choice(list)
             while c2 == c1:
                 c2 = random.choice(list)
-            print ('\t\t',c1.title()+'iggly'+c2+'uff')
+            xig = c1.title()+'iggly'+c2+'uff'
+            print ('\t\t',xig)
+            xigList.append(xig)
 
         end = time.time()
         elapsed = end - start
         
-        print('\nTime taken: ', elapsed, 'seconds.')
+        print('\nTime taken:', elapsed, 'seconds.')
+        print('\nProfessor Oak thinks', random.choice(xigList) ,'is a good choice.')
+        del xigList[:]
+        
     except ValueError:
         print('\nOnly integers are accepted. Sort your life out.')
